8f35782957fd79761040226c80967bad9a49416d
Sam Frederes <sfrederes@Sams-MacBook-Pro.local>
2017-07-17 11:21:07 -0500

    Lecture 1 donE

--- Caclulator/Caclulator/ViewController.swift
+++ Caclulator/Caclulator/ViewController.swift
@@ -10,113 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var result = 0;
-    var str1 = "0";
-    var str2 = "0";
-    var buttonChanged = false;
-
-    
-    @IBAction func Button_0(_ sender: UIButton) {
-        buttonChanged = true
-        if (Display.text! == "0"){Display.text! = "0"}
-        Display.text = Display.text! + "0"
-    }
-    
-    @IBAction func Button_1(_ sender: UIButton) {
-        if (Display.text! == "0"){
-            Display.text! = "1"
-        }
-        else{
-        Display.text! = Display.text! + "1"
-        }
-    }
-    
-  
-    @IBAction func Button_2(_ sender: UIButton) {
-        if (Display.text! == "0"){
-            Display.text! = "2"
-        }
-        else{
-            Display.text! = Display.text! + "2"
-        }
-    }
-   
-    @IBAction func Button_3(_ sender: UIButton) {
-        if (Display.text! == "0"){
-            Display.text! = "3"
-        }
-        else{
-        Display.text! = Display.text! + "3"
-        }
-    }
-    
-    @IBAction func Button_4(_ sender: UIButton) {
-        if (Display.text == "0"){
-            Display.text = "4"
-        }
-        else{
-        Display.text! = Display.text! + "4"
-        }
-    }
-    
-    @IBAction func Button_5(_ sender: UIButton) {
-        if (Display.text == "0"){
-            Display.text = "5"
-        }
-        else{
-        Display.text! = Display.text! + "5"
-        }
-    }
-    
-    @IBAction func Button_6(_ sender: UIButton) {
-        if (Display.text == "0"){
-            Display.text = "6"
-        }
-        else{
-        Display.text! = Display.text! + "6"
-        }
-    }
-    
-    @IBAction func Button_7(_ sender: UIButton) {
-        if (Display.text == "0"){
-            Display.text = "7"
-        }
-        else{
-        Display.text = Display.text! + "7"
-        }
-    }
-    
-    @IBAction func Button_8(_ sender: UIButton) {
-        if (Display.text == "0"){
-            Display.text = "8"
-        }
-        else{
-        Display.text = Display.text! + "8"
-        }
-    }
-    
-    @IBAction func Button_9(_ sender: UIButton) {
-        if (Display.text == "0"){
-            Display.text = "9"
-        }
-        else{
-        Display.text = Display.text! + "9"
-        }
-    }
-    
-    @IBAction func Equal(_ sender: UIButton) {
-        if (buttonChanged || str1 != "0"){}
-        else{}
-    }
-    
-    @IBAction func Clear(_ sender: UIButton) {
-        Display.text! = "0"
-    }
     
     @IBOutlet weak var Display: UILabel!
     
+    var result = "0"
     
+    @IBAction func Button_dig(_ sender: UIButton) {
+        if (Display.text! == "0"){
+            Display.text = sender.currentTitle!
+        }
+        else{
+            Display.text = Display.text! + sender.currentTitle!
+        }
+        result = Display.text!
+    }
     
+    var display_value : Double {
+        get {
+            return Double(Display.text!)!
+        }
+        set{
+           Display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func Button_op(_ sender: UIButton) {
+        if let mathmatical_symbol = sender.currentTitle {
+            switch mathmatical_symbol {
+            case "C":
+                Display.text = "0"
+                result = "0"
+            case "=":
+                Display.text = result
+            case "√":
+                display_value = sqrt(display_value)
+            default:
+                break
+            }
+        }
+    }
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
